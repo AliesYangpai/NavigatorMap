e9cbe19c264db8bab06db225f39e382748c2b6ee
AliesYangpai <yangpai_beibei@163.com>
2022-01-17 22:39:21 +0800

    hello 3d map

--- app/src/main/java/com/alie/navigatormap/MainActivity.kt
+++ app/src/main/java/com/alie/navigatormap/MainActivity.kt
@@ -2,10 +2,26 @@ package com.alie.navigatormap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.alie.navigatormap.databinding.ActivityMainBinding
+import com.amap.api.maps.model.MyLocationStyle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.mapView.onCreate(savedInstanceState)
+        initLocation()
     }
+
+
+    private fun initLocation() {
+        binding.mapView.map.myLocationStyle = MyLocationStyle().myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW)
+            .interval(2000).showMyLocation(true)
+        binding.mapView.map.isMyLocationEnabled = true
+    }
+
 }
--- app/src/main/java/com/alie/navigatormap/NavigatorApp.kt
+++ app/src/main/java/com/alie/navigatormap/NavigatorApp.kt
@@ -1,9 +1,12 @@
 package com.alie.navigatormap
 
 import android.app.Application
+import com.amap.api.maps.MapsInitializer
 
 class NavigatorApp:Application() {
     override fun onCreate() {
         super.onCreate()
+        MapsInitializer.updatePrivacyShow(this,true,true)
+        MapsInitializer.updatePrivacyAgree(this,true)
     }
 }
